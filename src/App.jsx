17f1335e33372f0c7648a0da3ecb046b03813d6c
tasnia-shahrin <tasniashahrin269@gmail.com>
2024-09-17 23:00:43 +0600

    tired

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
     </div>
   )
 }
