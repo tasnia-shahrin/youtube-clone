fc94ee1f47b3d0c0e1f89fd1ed923536d6c36728
tasnia-shahrin <tasniashahrin269@gmail.com>
2024-09-17 22:44:43 +0600

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Navbar.css'
+import menu_icon from '../assets/menu.png'
+import logo_icon from '../assets/logo.png'
+import search from '../assets/search.png'
+import upload from '../assets/upload.png'
+import more from '../assets/more.png'
+import notification from '../assets/notification.png'
+import user from '../assets/jack.png'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+        <div className="nav-left flex-div">
+            <img src={menu_icon} al t="" className='menu-icon'/>
+            <img src={logo_icon} alt="" className='logo-icon'/>
+        </div>
+        <div className="nav-middle flex-div">
+            <div className="search-box flex-div">
+                <input type="text" placeholder='Search' />
+                <img src={search} alt="" />
+            </div>
+            
+        </div>
+        <div className="nav-right flex-div">
+            <img src={upload} alt="" />
+            <img src={more} alt="" />
+            <img src={notification} alt="" />
+            <img src={user} alt="" className='user-icon'/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
